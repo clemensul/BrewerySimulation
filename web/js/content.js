c1ad460e98af0451ece170f5b42aaccef5366956
makra <makra@Marvin-Surface.localdomain>
2017-10-15 19:13:03 +0200

    startet input validation for user input in game.html

--- web/js/content.js
+++ web/js/content.js
@@ -19,6 +19,50 @@ function lobby (game_id, player, socket) {
     document.getElementById("lobby_player").textContent = "Spieler: " + JSON.stringify(player);
 }
 
-function game () {
+function game () { 
+    window.location.href = "game.html";
+   
+}
+
+var validparameter= function(){
+     // hier müssen nun alle input Felder überprüft werden und geschaut werden ob 
+    // alle eingaben valide sind
     
+     var mar_pl = 0;
+     if(parseInt($('#mar_pla').get(0).value)!==null){
+         mar_pl=parseInt($('#mar_pla').get(0).value);
+     };
+     var mar_tv = 0;
+     if(parseInt($('#mar_tvw').get(0).value)!==null){
+         mar_tv=parseInt($('#mar_tvw').get(0).value);
+     };
+     var mar_ra = parseInt($('#mar_rad').get(0).value);
+     
+     var for_bi = parseInt($('#for_bie').get(0).value);
+     var for_ch = parseInt($('#for_che').get(0).value);
+     var for_we = parseInt($('#for_wei').get(0).value);
+     
+     var kapital = parseInt($('#kapital').get(0).value);
+     var ausgaben = mar_pl + mar_tv + mar_ra + for_bi + for_ch + for_we;
+     
+     
+     
+     console.log(mar_pl);
+     console.log(mar_tv);
+     console.log(ausgaben);
+     console.log(kapital);
+     
+     if((kapital-ausgaben)<0){
+         alert("Zu viele Ausgaben");
+     }
+};
+
+
+
+function finish(){
+   
+    validparameter();
+    
+    
+   // window.location.href = "report.html";
 }
